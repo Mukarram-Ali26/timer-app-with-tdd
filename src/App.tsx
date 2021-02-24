bd1091a81c88735cdacb9a9f6f713dea915aee53
Mukarram-Ali26 <67237389+Mukarram-Ali26@users.noreply.github.com>
2021-02-24 08:40:23 +0500

    app finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Timer from "./components/Timer/Timer";
+import "./App.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="app_title"><i>Timer App by Mukarram Ali</i></h1>
+      <Timer />
     </div>
   );
 }
-
+//  yarn add enzyme @types/enzyme @types/enzyme-adapter-react-16 
+// enzyme-adapter-react-16 enzyme-to-json jest-fetch-mock ts-jest
 export default App;
